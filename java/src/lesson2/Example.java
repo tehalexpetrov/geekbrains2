cbcb87b3dd96818ccce05c9d3c2fb4f5a55df430
Alexander <tehalexpetrov@gmail.com>
2020-12-26 15:22:51 +0300

    Домашнее задание воторого урока

---
+++ java/src/lesson2/Example.java
@@ -0,0 +1,6 @@
+package lesson2;
+
+public class Example {
+    private static final int MassI = 4;
+    private static final int MassJ = 4;
+}
--- java/src/lesson2/Main.java
+++ java/src/lesson2/Main.java
@@ -1,7 +1,36 @@
 package lesson2;
 
 public class Main {
-    public static void main(String[] args) {
 
+    private static final int MassI = 4;
+    private static final int MassJ = 4;
+    private static final String point = String.valueOf('*');
+
+
+    public static void main(String[] args) {
+        String[][] massArray = new String[MassI][MassJ];
+        try {
+
+
+
+        } catch (Exception e) {
+
+        }
     }
+
+    public void stringExection(String[][] massArray) throws MyArraySizeException {
+        if(massArray.length != MassI || massArray.length != MassJ ) throw new MyArraySizeException("Неверное количество строк"); {
+            for (int i = 0; i < massArray.length; i++) {
+                if (massArray[i].length != MassI || massArray.length != MassJ) throw new MyArraySizeException(String.format("Неверное количество столбцов в %d-й" + " строке", i));{
+                    for (int j = 0; j < massArray[i].length; j++) {
+                        System.out.print(massArray[i][j]);
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+
+
 }
