f2709adc1c7c5646225b0dcde85fd56792593221
Alexander <tehalexpetrov@gmail.com>
2021-01-20 07:07:12 +0300

    Домашняя работа пятого урока готова

--- java/src/lesson5/Main.java
+++ java/src/lesson5/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Main test = new Main();
             test.oneMethod();
+            test.twoMethod();
     }
 
     //Создаем первый метод
@@ -27,6 +28,37 @@ public class Main {
     }
 
     //Создаем второй метод
+        public void twoMethod(){
+            float[] arr = new float[size];
+            float[] arr1 = new float[h];
+            float[] arr2 = new float[h];
 
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = 1.0f;
+            }
+
+            long a = System.currentTimeMillis();
+            System.arraycopy(arr,0, arr1,0,h);
+            System.arraycopy(arr, h, arr2, 0,h);
+
+            new Thread(){
+                public void run(){
+                    float[] a1 = calcArray(arr1);
+                    System.arraycopy(a1, 0, arr1, 0, a1.length);
+                }
+            }.start();
+
+            new Thread() {
+                public void run() {
+                    float[] a2 = calcArray(arr2);
+                    System.arraycopy(a2, 0, arr2, 0, a2.length);
+                }
+            }.start();
+
+            System.arraycopy(arr1, 0, arr, 0, h);
+            System.arraycopy(arr2, 0, arr, h, h);
+            System.out.println("Время выполнения второго метода: " + (System.currentTimeMillis() - a));
+
+        }
 
 }
